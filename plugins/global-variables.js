e61b964206f3c86f6d0b3d22fec53459dbf58d57
Leon <leonfvogler@gmail.com>
2021-06-09 00:42:14 +0200

    update api base path

--- plugins/global-variables.js
+++ plugins/global-variables.js
@@ -3,13 +3,6 @@ Vue.prototype.$projectRoute = {
   slug: "",
   path: "/project/",
   fullPath: "/",
-  // changeRoute (slug) {
-  //   if (this.slug === slug) return
-  //   this.slug = slug
-  //   this.fullPath = slug ? this.path + this.slug : '/'
-  //   history.pushState({}, null, this.fullPath)
-  // }
 };
-// Vue.prototype.$apiBasePath = 'https://api.jonasvoglermusic.de/wp-json/wp/v2/'
-Vue.prototype.$apiBasePath = 'https://jonasvoglermusic.com/wp-json/wp/v2/'
+Vue.prototype.$apiBasePath = 'https://api.jonasvoglermusic.de/wp-json/wp/v2/'
 
